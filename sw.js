dd915cbc20b66a42d37c4f01ca3e1c0409a6498e
eletroshopp02-beep <eletroshopp02@gmail.com>
2026-09-26 10:22:14 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,16 @@
-const CACHE_NAME = "eletroshopp-v2";
-
-const ARQUIVOS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ARQUIVOS))
-  );
-  self.skipWaiting();
+const CACHE = 'eletroshopp-v2';
+const CORE = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
 });
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(chaves => Promise.all(
-      chaves.filter(chave => chave !== CACHE_NAME).map(chave => caches.delete(chave))
-    ))
-  );
-  self.clients.claim();
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(
-    caches.match(event.request).then(resposta => resposta || fetch(event.request))
-  );
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+    return response;
+  }).catch(() => caches.match('./index.html'))));
 });
