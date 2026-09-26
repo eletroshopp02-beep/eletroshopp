9b9e18712c6ea4c9c3556438fd02f72ab65ef466
eletroshopp02-beep <eletroshopp02@gmail.com>
2026-09-26 09:41:16 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,39 +1,32 @@
-const CACHE_NAME = "eletroshopp-v1";
+const CACHE_NAME = "eletroshopp-v2";
 
 const ARQUIVOS = [
   "./",
   "./index.html",
-  "./manifest.webmanifest"
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ARQUIVOS);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ARQUIVOS))
   );
-
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(chaves => {
-      return Promise.all(
-        chaves
-          .filter(chave => chave !== CACHE_NAME)
-          .map(chave => caches.delete(chave))
-      );
-    })
+    caches.keys().then(chaves => Promise.all(
+      chaves.filter(chave => chave !== CACHE_NAME).map(chave => caches.delete(chave))
+    ))
   );
-
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
   event.respondWith(
-    caches.match(event.request).then(resposta => {
-      return resposta || fetch(event.request);
-    })
+    caches.match(event.request).then(resposta => resposta || fetch(event.request))
   );
 });
